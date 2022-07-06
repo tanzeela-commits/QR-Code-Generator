9d55ac33dcb080c6b5e636f97af4d8d409302714
tanzeela <90628873+tanzeela-commits@users.noreply.github.com>
2022-07-06 19:38:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const wrapper = document.querySelector(".wrapper"),
+  qrInput = wrapper.querySelector(".form input"),
+  generateBtn = wrapper.querySelector(".form button"),
+  qrImg = wrapper.querySelector(".qr-code img");
+
+generateBtn.addEventListener("click", () => {
+  let qrValue = qrInput.value;
+  if (!qrValue) return; //if the input is empty then return from here
+  generateBtn.innerText = "generating qr code...";
+  //getting a qr code of user entered value using the qr server
+  //api and passing the api returned img src to qrImg
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+  qrImg.addEventListener("load", () => {
+    //once qr code img loaded
+    wrapper.classList.add("active");
+    generateBtn.innerText = "generate qr code";
+  });
+});
+
+qrInput.addEventListener('keyup',()=>{
+    if(!qrInput.value){
+        wrapper.classList.remove('active');
+    }
+});
